1d207900753850d5c746d90d9456b942042a0877
Bob-Hinkley <cb@cbs-MacBook-Air.local>
2017-01-06 10:45:24 -0600

    edited .js file for assignment

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ function Student(firstName, favoriteColor) {
  * that represent you and your coding partner(s).
  */
 
+ var erik = new Student('Erik', 'Grey');
+ var bob = new Student('Bob', 'Red');
+
 /**
   * 2. Create a function that takes one argument. That argument will
   * represent a single student object. The function will log the student's
   * name.
   */
+function namer(subject){
+  console.log(subject.firstName);
+}
 
 /***** SWITCH PROGRAMMERS *****/
 
@@ -21,10 +27,14 @@ function Student(firstName, favoriteColor) {
   * represent a single student object. The function will log the student's
   * favoriteColor.
   */
-
+function colorer(subject){
+  console.log(subject.favoriteColor);
+}
 /**
   * 4. Put both students created in Step 1 into an array called students.
   */
+var students=[erik,bob];
+
 
 /***** SWITCH PROGRAMMERS *****/
 
@@ -32,3 +42,8 @@ function Student(firstName, favoriteColor) {
  * 5. Loop over the students array and invoke both functions from Step 2 and
  * Step 3. You should see console logs for each students firstName and favoriteColor.
  */
+
+ for (var i = 0; i < students.length; i++ ) {
+   namer(students[i]);
+   colorer(students[i]);
+ }
